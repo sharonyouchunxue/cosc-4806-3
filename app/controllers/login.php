35a98f20bcd152b0f0a1351a022f86c865d7dfcc
sharonyouchunxue <102676646+sharonyouchunxue@users.noreply.github.com>
2024-06-11 16:54:55 +0000

    add unsuccessful log

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -2,16 +2,16 @@
 
 class Login extends Controller {
 
-    public function index() {		
-	    $this->view('login/index');
-    }
-    
-    public function verify(){
+		public function index() {		
+			$this->view('login/index');
+		}
+
+		public function verify(){
 			$username = $_REQUEST['username'];
 			$password = $_REQUEST['password'];
-		
+
 			$user = $this->model('User');
 			$user->authenticate($username, $password); 
-    }
+		}
 
 }
